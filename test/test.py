3bc5889a741ed5387f321b92814356b89a286340
hadinasser21 <80849023+hadinasser21@users.noreply.github.com>
2026-01-17 19:53:41 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -25,31 +25,31 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test 
- 	dut.ui_in[0].value = 0
+    # Test case 1
+    dut.ui_in[0].value = 0
     dut.ui_in[1].value = 0
     dut.ui_in[2].value = 0
 
-	 # Wait for a set number of clock cycles to see the output values
-	 # In this case, 25 clock cycles
     await ClockCycles(dut.clk, 25)
-    
-	 # Assert the actual expected output of your module
- assert dut.uo_out[0].value == 1
-    assert dut.uo_out[1].value == 1
 
-	 # Repeat for other test cases
-    dut.ui_in[0].value = 0
-    dut.ui_in[1].value = 0
-    dut.ui_in[2].value = 1
-    await ClockCycles(dut.clk, 25)
-    assert dut.uo_out[0].value == 0
-    assert dut.uo_out[1].value == 0
-
-    dut.ui_in[0].value = 0
-    dut.ui_in[1].value = 1
-    dut.ui_in[2].value = 0
-    await ClockCycles(dut.clk, 25)
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 1
 
+    # Test case 2
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 0
+    dut.ui_in[2].value = 1
+
+    await ClockCycles(dut.clk, 25)
+
+    assert dut.uo_out[0].value == 0
+    assert dut.uo_out[1].value == 0
+
+    # Test case 3
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 1
+    dut.ui_in[2].value = 0
+
+    await ClockCycles(dut.clk, 25)
+
+    assert dut.uo_ou_
